f713dbc19a670e463ef04c777aea0a6f0f3e82e9
Xavier Francis <xavychan@gmail.com>
2022-07-02 10:01:13 +0300

    modifiied dependencies and hello message

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,7 @@
     export default {
         data() {
             return {
-                message: "Xavier Francis Antony Manuel",
+                message: "Hello Vue World",
             }
         }
     }
