db893f0706d949eddd48e2cbdbbacfbcabf35344
Xavier Francis <xavychan@gmail.com>
2022-07-03 16:18:04 +0300

    using it as component

--- src/main.js
+++ src/main.js
@@ -1,4 +1,6 @@
 import * as Vue from "vue"
 import App from "./App.vue"
 
- Vue.createApp(App).mount('#app');
+ //Vue.createApp(App).mount('#app');
+const wc = Vue.defineCustomElement(App);
+ customElements.define('vue-helloworld', wc);
